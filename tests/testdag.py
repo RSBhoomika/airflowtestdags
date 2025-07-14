18d3a4c5cdd8cad0701bcc01fde76830928714dc
RSBhoomika <72779921+RSBhoomika@users.noreply.github.com>
2025-07-14 13:58:31 +0530

    Update remove dag testdag.py

--- tests/testdag.py
+++ tests/testdag.py
@@ -1,26 +1,26 @@
-from airflow import DAG
-from airflow.operators.python import PythonOperator
-from datetime import datetime
+# from airflow import DAG
+# from airflow.operators.python import PythonOperator
+# from datetime import datetime
 
-# Define the task function
-def say_hello():
-    print("Hello, Airflow!")
+# # Define the task function
+# def say_hello():
+#     print("Hello, Airflow!")
 
-# Define the DAG
-with DAG(
-    dag_id='example_hello_world',
-    start_date=datetime(2024, 1, 1),
-    schedule_interval=None,  # Only run manually
-    catchup=False,
-    tags=["example"],
-) as dag:
+# # Define the DAG
+# with DAG(
+#     dag_id='example_hello_world',
+#     start_date=datetime(2024, 1, 1),
+#     schedule_interval=None,  # Only run manually
+#     catchup=False,
+#     tags=["example"],
+# ) as dag:
 
-    hello_task = PythonOperator(
-        task_id='say_hello_task',
-        python_callable=say_hello,
-    )
+#     hello_task = PythonOperator(
+#         task_id='say_hello_task',
+#         python_callable=say_hello,
+#     )
 
-    hello_task
+#     hello_task
 
 
 
