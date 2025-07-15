fed8dd99dfef48f18e3ca8bda9137da5a38d6dbb
RSBhoomika <72779921+RSBhoomika@users.noreply.github.com>
2025-07-15 20:16:23 +0530

    Update emit_openmetadata_lineage.py

--- tests/scripts/emit_openmetadata_lineage.py
+++ tests/scripts/emit_openmetadata_lineage.py
@@ -10,17 +10,17 @@ def emit_column_lineage_to_om():
 
     # Connection config
     server_config = OpenMetadataConnection(
-        hostPort="http://35.203.40.184:31291/api",
+        hostPort="http://100.94.70.9:32325/api",
         authProvider="openmetadata",
         securityConfig=OpenMetadataJWTClientConfig(
-            jwtToken="eyJraWQiOiJHYjM4OWEtOWY3Ni1nZGpzLWE5MmotMDI0MmJrOTQzNTYiLCJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJvcGVuLW1ldGFkYXRhLm9yZyIsInN1YiI6ImF1dG9waWxvdGFwcGxpY2F0aW9uYm90Iiwicm9sZXMiOltudWxsXSwiZW1haWwiOiJhdXRvcGlsb3RhcHBsaWNhdGlvbmJvdEBvcGVubWV0YWRhdGEub3JnIiwiaXNCb3QiOnRydWUsInRva2VuVHlwZSI6IkJPVCIsImlhdCI6MTc1MDg1NTYyMywiZXhwIjpudWxsfQ.LFVF6sq6yoXv-1uZSpPXymLuzutPW_V5NEZE446LCTSGkgXkQyTi4fN0LTjJcgMqth2I79cW9A0ue7dMIkW3_sKqYHYeCSb8dpSjzmjk-x_RYE1yRb6y9Sd6FigZhmrQHlVeoaun9NO9N6bYgXagXROqpnt94yqQi7ZwXV4nV-3Jd99ueHtsLll0pH9vEPI5eBeD1YwHaAEZyhBZhOGIhlb2PppbBL4wfMScD-SftgSGYvNEVqQvuvgIqQAU2PVTYChKLyhQW4m51rf1poHT7gtwJyLagYfEXbxOWg6gWoMNSCQeiuAPJdV7Tu5VqW73eI1pe5KYT742tNM9u4QRqw"
+            jwtToken="eyJraWQiOiJHYjM4OWEtOWY3Ni1nZGpzLWE5MmotMDI0MmJrOTQzNTYiLCJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJvcGVuLW1ldGFkYXRhLm9yZyIsInN1YiI6ImF1dG9waWxvdGFwcGxpY2F0aW9uYm90Iiwicm9sZXMiOltudWxsXSwiZW1haWwiOiJhdXRvcGlsb3RhcHBsaWNhdGlvbmJvdEBvcGVubWV0YWRhdGEub3JnIiwiaXNCb3QiOnRydWUsInRva2VuVHlwZSI6IkJPVCIsImlhdCI6MTc1MjU4MDg4OCwiZXhwIjpudWxsfQ.ThRsRbcJLTS0lLeik1zENF8QvCKx4H1XXuFystGkWT-OkWCMub9mHoSahJJv8P-A6ONELberfcqsduVvTIkMmbQNGGfyRgoKEKhi01--iB2nOFsqlWDjK7WpTpaHmaAUdK3RsSuTg8jSeDYpMfXn_9CqlTto-XGEweRCPkVAYeccvUZ5x9XgKbXgYG4T6Wh6CGwtKhErta6dHv-ngxNYHoOFMheIsUqG1HOSVcjYR97q_rg4a4AkSwt98VvcA3sv_nnG5aJr4UDpu3z8NMSZjKZ-CXJHkHnF_mLM51P2uAKXIYQYcaN1yqrTlxiecKiHNyYC39aXPXVJDKtRyKIcGA"
         ),
     )
 
     metadata = OpenMetadata(server_config)
 
     # FQNs
-    source_fqn = "MinIO.minio-data-source.minio-source/sales"
+    source_fqn = "openmetadata-minio.openmetadata.minio-source/sales"
     target_fqn = "demo_environment.default.demo_database.raw_sales"
 
     # Fetch entities
