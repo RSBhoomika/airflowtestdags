79ef083d3d04ae9abb4c11f6c95f8377d3ae892c
RSBhoomika <72779921+RSBhoomika@users.noreply.github.com>
2025-07-15 14:08:06 +0530

    Create brokerload1m.py

---
+++ tests/brokerload1m.py
@@ -0,0 +1,71 @@
+from airflow import DAG
+from airflow.operators.mysql_operator import MySqlOperator
+from datetime import datetime
+
+# Default DAG arguments
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'retries': 0,
+}
+
+doris_user = "test_user"
+doris_password = "password"
+doris_host = "100.94.70.9"
+doris_port = "31161"
+
+db_name = "test"
+table_name = "table2"
+file_path = "s3://airflow-test/TrafficData.csv"
+
+# Broker Load SQL for MinIO (S3-compatible)
+broker_name = "s3_broker"
+broker_props = {
+    "aws_access_key": "minio",
+    "aws_secret_key": "minio123",
+    "endpoint": "http://100.94.70.9:31677"
+}
+broker_props_sql = ", ".join([f'"{k}"="{v}"' for k, v in broker_props.items()])
+
+load_sql = f'''
+LOAD LABEL {db_name}.airflow_broker_load_{{{{ ts_nodash }}}}
+(
+    DATA INFILE("{file_path}")
+    INTO TABLE {table_name}
+    FORMAT AS "csv"
+    (COLUMNS TERMINATED BY ",")
+)
+WITH BROKER "{broker_name}"
+(
+    {broker_props_sql}
+)
+PROPERTIES
+(
+    "timeout" = "600",
+    "max_filter_ratio" = "0.1"
+    -- "compress_type" = "GZ"  # Remove or comment out for plain CSV
+);
+'''
+
+with DAG(
+    dag_id='doris_broker_load_1m',
+    default_args=default_args,
+    schedule_interval=None,
+    start_date=datetime(2025, 7, 11),
+    catchup=False,
+    description='Broker Load to Doris from MinIO',
+    tags=['doris', 'broker_load'],
+) as dag:
+
+    broker_load_task = MySqlOperator(
+        task_id='broker_load_to_doris',
+        sql=load_sql,
+        autocommit=True,
+        host=doris_host,
+        user=doris_user,
+        password=doris_password,
+        database=db_name,
+        port=31115,  # Use Doris FE MySQL-compatible port
+    )
+
+    broker_load_task 
