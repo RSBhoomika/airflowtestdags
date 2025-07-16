f9c9f751ee142823168c9ae951a7a2dcd6880d5b
RSBhoomika <72779921+RSBhoomika@users.noreply.github.com>
2025-07-16 16:22:19 +0530

    Update 2.0 1msparkconnectingestion.py

--- tests/1msparkconnectingestion.py
+++ tests/1msparkconnectingestion.py
@@ -67,61 +67,6 @@ def upload_to_s3():
         log.error(f"Error running Spark Connect upload job: {e}", exc_info=True)
         raise
 
-def delete_s3_path():
-    logging.basicConfig(level=logging.INFO)
-    log = logging.getLogger(__name__)
-    log.info(f"Deleting S3 path: {S3_PATH}")
-
-    try:
-        spark = create_spark_session()
-
-        # Delete path using S3 API or Hadoop FS command outside JVM
-        # Since Spark Connect does not support JVM access,
-        # use an alternative method like boto3 or minio client here
-        import boto3
-        from botocore.exceptions import ClientError
-
-        # Configure boto3 client for MinIO
-        s3_client = boto3.client(
-            's3',
-            endpoint_url="http://100.94.70.9:31677",
-            aws_access_key_id="minio",
-            aws_secret_access_key="minio123",
-            region_name='us-east-1',
-            config=boto3.session.Config(signature_version='s3v4'),
-            use_ssl=False
-        )
-
-        # Extract bucket and key prefix from S3_PATH
-        # S3_PATH example: "s3a://airflow-test/sparkconnect/"
-        path_without_scheme = S3_PATH.replace("s3a://", "")
-        bucket, prefix = path_without_scheme.split('/', 1)
-
-        # List and delete all objects under the prefix
-        paginator = s3_client.get_paginator('list_objects_v2')
-        pages = paginator.paginate(Bucket=bucket, Prefix=prefix)
-
-        objects_to_delete = []
-        for page in pages:
-            if "Contents" in page:
-                for obj in page["Contents"]:
-                    objects_to_delete.append({'Key': obj['Key']})
-
-        if objects_to_delete:
-            delete_response = s3_client.delete_objects(
-                Bucket=bucket,
-                Delete={'Objects': objects_to_delete}
-            )
-            log.info(f"Deleted objects under {S3_PATH}: {delete_response}")
-        else:
-            log.info(f"No objects found to delete at {S3_PATH}")
-
-        spark.stop()
-
-    except Exception as e:
-        log.error(f"Error deleting S3 path: {e}", exc_info=True)
-        raise
-
 
 with DAG(
     dag_id='spark_connect_minio_ingestion_one_million',
@@ -138,15 +83,6 @@ with DAG(
         task_id='upload_to_s3',
         python_callable=upload_to_s3,
     )
-
-    sleep_task = BashOperator(
-        task_id='sleep_20_seconds',
-        bash_command='sleep 20',
-    )
-
-    delete_task = PythonOperator(
-        task_id='delete_s3_path',
-        python_callable=delete_s3_path,
-    )
+    
 
     upload_task 
